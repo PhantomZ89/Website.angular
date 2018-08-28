38564368f09dac40069a4ebfc7ae0ac5266a2ab7
Ceddy ST <KidChaor89@Ceddys-MacBook-Air.local>
2018-08-27 21:01:55 -0400

    created new home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
